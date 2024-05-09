43691b1a131192685e07bf6d5ef4ecb5e3f35376
Fabiana Tavares <C0653673@vale.com>
2024-05-09 14:13:57 -0300

    feat: first part apply array map by archive data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 import Hero from './components/Hero';
+import data from './data';
+console.log(data);
 
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card
-        img="kate.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Card(props) {
   return (
     <div className="card">
-      <img src={`../images/${props.img}`} alt="logo" className="card--image" />
+      <img src={`${props.img}`} alt="logo" className="card--image" />
       <div className="card-stats">
-        <img src="../images/stars.png" className="card--star" />
+        <img src="../images/stars.png" className="card--star" alt="stars" />
         <span>{props.rating}</span>
         <span className="grey">({props.reviewCount}) . </span>
         <span className="grey">{props.country}</span>
