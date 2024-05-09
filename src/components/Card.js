5ad474c702a5cf8c91401ee7582f4ab264c3a59c
Fabiana Tavares <C0653673@vale.com>
2024-05-09 13:20:17 -0300

    feat: apply props

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,16 @@ import React from 'react';
 export default function Card(props) {
   return (
     <div className="card">
-      <img src="../images/kate.png" alt="logo" className="card--image" />
+      <img src={`../images/${props.img}`} alt="logo" className="card--image" />
       <div className="card-stats">
         <img src="../images/stars.png" className="card--star" />
-        <span>5.0</span>
-        <span className="grey">(6) . </span>
-        <span className="grey">USA</span>
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount}) . </span>
+        <span className="grey">{props.country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${136}</span> / person
       </p>
     </div>
   );
